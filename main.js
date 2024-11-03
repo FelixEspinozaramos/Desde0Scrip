f48381745bbe3d3cf5b364fa8f5ec03943665202
Felix <felixespinozaramos.96@gmail.com>
2024-11-03 18:30:20 -0500

    primer java

---
+++ main.js
@@ -0,0 +1,25 @@
+//console.log("felix")
+//console.log(3)
+//console.log(true)
+
+//alert("hola mundo")
+
+//console.error("suma error",resultado);
+//console.warn("suma mas", resultado);
+
+
+//Definir Variables
+
+const numero1 = 3;
+const numero2 = 2;
+
+// Declarar la funcion
+
+let suma = 0;
+let producto = 0;
+
+resultado = numero1 + numero2;
+producto = numero1*numero2;
+
+console.log(resultado)
+console.log(producto)
